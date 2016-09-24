da4ebe88d19256c76a40f096459b4ac84eabc8d1
deepikak02 <dkrish15@asu.edu>
2016-09-23 22:13:38 -0700

    Login Activity setup

--- Android/SmartShopper/app/src/main/java/com/example/android/smartshopper/Splash.java
+++ Android/SmartShopper/app/src/main/java/com/example/android/smartshopper/Splash.java
@@ -1,15 +1,15 @@
 package com.example.android.smartshopper;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by deepika on 9/23/2016.
  */
-public class Splash extends Activity {
+public class Splash extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +18,7 @@ public class Splash extends Activity {
         new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent i = new Intent(Splash.this, MainActivity.class);
+                Intent i = new Intent(Splash.this, LoginActivity.class);
                 startActivity(i);
             }
         }, 3000);
