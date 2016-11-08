eb558fa5df488652a4722d7c6a2757da056b7731
Digant Jagtap <digantjgtp@gmail.com>
2016-11-08 00:44:42 -0700

    Fixed Navigation drawer

--- Android/SmartShopper/app/src/main/java/com/group28/android/smartshopper/Activity/HomeActivity.java
+++ Android/SmartShopper/app/src/main/java/com/group28/android/smartshopper/Activity/HomeActivity.java
@@ -336,19 +336,21 @@ public class HomeActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_manage) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_share) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_logout) {
+            // Signout from the application
+            signOut();
+        } /* else if (id == R.id.nav_manage) {
 
         } else if (id == R.id.nav_share) {
 
         } else if (id == R.id.nav_send) {
 
-        }
+        } */
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
