4676e007575d5938d5f1d64f46388eb23dbbeff3
Mihir Bhatt <mbbhatt@asu.edu>
2016-11-16 01:34:48 -0700

    GeoFence + Place Web Service 	modified:   Android/SmartShopper/.idea/gradle.xml 	modified:   Android/SmartShopper/app/build.gradle 	modified:   Android/SmartShopper/app/src/main/AndroidManifest.xml 	modified:   Android/SmartShopper/app/src/main/java/com/group28/android/smartshopper/Activity/HomeActivity.java 	modified:   Android/SmartShopper/app/src/main/java/com/group28/android/smartshopper/Activity/MainActivity.java 	modified:   Android/SmartShopper/build.gradle 	Android/SmartShopper/.idea/inspectionProfiles/ 	Android/SmartShopper/app/src/main/java/com/group28/android/smartshopper/AppController.java 	Android/SmartShopper/app/src/main/java/com/group28/android/smartshopper/Model/Place.java 	Android/SmartShopper/app/src/main/java/com/group28/android/smartshopper/Service/GeoFenceService.java 	Android/SmartShopper/app/src/main/java/com/group28/android/smartshopper/Service/GeofenceTransitionsReceiver.java

--- Android/SmartShopper/app/src/main/java/com/group28/android/smartshopper/Activity/MainActivity.java
+++ Android/SmartShopper/app/src/main/java/com/group28/android/smartshopper/Activity/MainActivity.java
@@ -1,14 +1,17 @@
 package com.group28.android.smartshopper.Activity;
 
+import android.Manifest;
 import android.app.ProgressDialog;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -165,6 +168,7 @@ public class MainActivity extends AppCompatActivity  implements
         catch(IOException e){
             Log.e("error","DBHelper getInstance error");
         }
+
     }
 
     @Override
