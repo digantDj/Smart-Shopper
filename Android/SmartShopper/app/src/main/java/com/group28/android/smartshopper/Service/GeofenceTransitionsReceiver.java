bc814a420b7dfbc62ba712035b5a4de9938c04cb
purukaushik <purukaushik@asu.edu>
2016-11-17 21:30:41 -0700

    Adding transitions receiver

--- Android/SmartShopper/app/src/main/java/com/group28/android/smartshopper/Service/GeofenceTransitionsReceiver.java
+++ Android/SmartShopper/app/src/main/java/com/group28/android/smartshopper/Service/GeofenceTransitionsReceiver.java
@@ -15,6 +15,8 @@ import android.util.Log;
 import com.google.android.gms.location.Geofence;
 import com.google.android.gms.location.GeofenceStatusCodes;
 import com.google.android.gms.location.GeofencingEvent;
+import com.group28.android.smartshopper.Activity.HomeActivity;
+import com.group28.android.smartshopper.R;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,6 +29,10 @@ public class GeofenceTransitionsReceiver extends BroadcastReceiver {
 
     protected static final String TAG = "GeofenceTransitionsReceiver";
 
+    private static final String GROUP_ID = "SmartShopNotifs";
+
+    private static int generatedId = 0;
+
     @Override
     public void onReceive(Context context, Intent intent) {
         GeofencingEvent event = GeofencingEvent.fromIntent(intent);
@@ -35,73 +41,67 @@ public class GeofenceTransitionsReceiver extends BroadcastReceiver {
             return;
         }
 
-        String description = getGeofenceTransitionDetails(event);
-        Log.i(TAG, description);
-        sendNotification(description, context);
-    }
-
-    /*@Override
-    protected void onHandleIntent(Intent intent) {
-        GeofencingEvent event = GeofencingEvent.fromIntent(intent);
-        if (event.hasError()) {
-            Log.e(TAG, "GeofencingEvent Error: " + event.getErrorCode());
-            return;
+        String requestId = event.getTriggeringGeofences().get(0).getRequestId();
+        int transition = event.getGeofenceTransition();
+        switch (transition) {
+            case Geofence.GEOFENCE_TRANSITION_ENTER: {
+                Log.i(TAG, "Entered " + requestId);
+                sendNotification("You're near " + requestId + ". Do you want to buy something?", context);
+                break;
+            }
+            case Geofence.GEOFENCE_TRANSITION_EXIT: {
+                Log.i(TAG, "Exit from " + requestId);
+                //sendNotification("Exit from " + requestId,context);
+                break;
+            }
+            case Geofence.GEOFENCE_TRANSITION_DWELL: {
+                Log.i(TAG, "Dwelling at " + requestId );
+                sendNotification("You were at " + requestId + ". Did you buy?", context);
+                break;
+            }
+            default: {
+                Log.i(TAG, "Default");
+                break;
+            }
         }
-
-        String description = getGeofenceTransitionDetails(event);
-        sendNotification(description);
-    }*/
-
-    private static String getGeofenceTransitionDetails(GeofencingEvent event) {
-        String transitionString =
-                GeofenceStatusCodes.getStatusCodeString(event.getGeofenceTransition());
-        List triggeringIDs = new ArrayList();
-        for (Geofence geofence : event.getTriggeringGeofences()) {
-            triggeringIDs.add(geofence.getRequestId());
-        }
-        return String.format("%s: %s", transitionString, TextUtils.join(", ", triggeringIDs));
     }
 
     private void sendNotification(String notificationDetails, Context context) {
-        // Create an explicit content Intent that starts MainActivity.
-       /* Intent notificationIntent = new Intent(getApplicationContext(), MainActivity.class);
+        // Create an explicit content Intent that starts the main Activity.
+        Intent notificationIntent = new Intent(context, HomeActivity.class);
+
+        // Construct a task stack.
+        android.app.TaskStackBuilder stackBuilder = android.app.TaskStackBuilder.create(context);
+
+        // Add the main Activity to the task stack as the parent.
+        stackBuilder.addParentStack(HomeActivity.class);
+
+        // Push the content Intent onto the stack.
+        stackBuilder.addNextIntent(notificationIntent);
 
         // Get a PendingIntent containing the entire back stack.
-        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
-        stackBuilder.addParentStack(MainActivity.class).addNextIntent(notificationIntent);
         PendingIntent notificationPendingIntent =
                 stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
 
         // Get a notification builder that's compatible with platform versions >= 4
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+        android.support.v4.app.NotificationCompat.Builder builder = new android.support.v4.app.NotificationCompat.Builder(context);
 
         // Define the notification settings.
-        builder.setColor(Color.RED)
-                .setContentTitle(notificationDetails)
-                .setContentText("Click notification to return to App")
+        builder.setSmallIcon(R.drawable.cast_ic_notification_small_icon)
+                .setColor(Color.RED)
+                .setContentTitle("SmartShopper")
+                .setContentText(notificationDetails)
                 .setContentIntent(notificationPendingIntent)
-                .setAutoCancel(true);
+                .setGroup(GROUP_ID);
 
-        // Fire and notify the built Notification.
-        NotificationManager notificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        notificationManager.notify(0, builder.build());*/
+        // Dismiss notification once the user touches it.
+        builder.setAutoCancel(true);
 
-        Log.i(TAG, "Sending Notification....");
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
-
-        // Define the notification settings.
-        builder.setColor(Color.RED)
-                .setContentTitle(notificationDetails)
-                .setContentText("Click notification to return to App")
-                //.setContentIntent(notificationPendingIntent)
-                .setAutoCancel(true);
-
-        // Fire and notify the built Notification.
-        NotificationManager notificationManager =
+        // Get an instance of the Notification manager
+        NotificationManager mNotificationManager =
                 (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-        notificationManager.notify(0, builder.build());
-        Log.i(TAG, "Notification Sent");
-    }
 
+        // Issue the notification
+        mNotificationManager.notify(generatedId++, builder.build());
+    }
 }
